16af47411c91b25c61f89ce6e4f31aeecb7f46ec
hatch7s1 <77197965+hatch7s1@users.noreply.github.com>
2021-01-09 22:26:19 +0700

    delete exampleEnty

--- scripts/main.js
+++ scripts/main.js
@@ -160,7 +160,6 @@ function clearRanking() {
 function populateTable(trainees) {
   // Currently just duplicates the first table entry
   let table = document.getElementById("table__entry-container");
-  exampleEntry = table.children[0];
   for (let i = 0; i < trainees.length; i++) {
     // generate and insert the html for a new trainee table entry
     table.insertAdjacentHTML("beforeend", populateTableEntry(trainees[i]));
